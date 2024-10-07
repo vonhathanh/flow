25ce5db5152822a0d1b5229faa2ef9d33bc67ba9
hanhvn <vonhathanh@gmail.com>
2024-10-07 17:29:57 +0700

    refactor codes to allows multiprocessing

--- flow/main.py
+++ flow/main.py
@@ -1,40 +1,47 @@
+import asyncio
+import multiprocessing
+import os
+
 import pyautogui
 import numpy as np
-import time
 import whisper
 
-from utils import process_recorded_speech, remove_audio, save_audio
+from glob import glob
+from os.path import join
+
+from utils import process_recorded_speech, remove_audio, save_audio, is_silent
 from flow.ignored_words import IGNORED_WORDS
 from flow.configs import *
 
 model = whisper.load_model("tiny.en")
 
+# a flag to indicate the program has been stopped, for future use
+is_stopped = False
 
-def is_silent(data_chunk):
-    """Returns 'True' if below the 'silent' threshold"""
-    return max(data_chunk) < THRESHOLD
+os.makedirs(RECORDINGS_DIR, exist_ok=True)
 
 
-def record_audio():
+async def record_audio():
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-
-    print("Listening... Speak to start recording.")
 
     audio_buffer = []
     silent_chunks = 0
     audio_started = False
 
+    print("Listening... Speak to start recording.")
+
     while True:
         data = stream.read(CHUNK)
-        audio_data = np.frombuffer(data, dtype=np.int16)
+        raw_audio_data = np.frombuffer(data, dtype=np.int16)
 
-        silent = is_silent(audio_data)
-        print("is silent ", silent)
+        silent = is_silent(raw_audio_data)
+        # print("is silent ", silent)
 
         if audio_started:
             audio_buffer.append(data)
 
+        # user starts talking
         if not silent and not audio_started:
             print("Recording started...")
             audio_started = True
@@ -43,41 +50,59 @@ def record_audio():
         elif silent and audio_started:
             silent_chunks += 1
             if silent_chunks > SILENCE_LIMIT * (RATE / CHUNK):
-                break
+                # user has stopped talking, saves recording to file
+                silent_chunks = 0
+                audio_started = False
+                save_audio(audio_buffer)
+                audio_buffer = []
+        # user still talking
         else:
             silent_chunks = 0
 
-    print("Finished recording.")
+        if is_stopped:
+            break
+
+        await asyncio.sleep(0.01)
 
     stream.stop_stream()
     stream.close()
     p.terminate()
 
-    return audio_buffer
+
+async def process_new_audio_files():
+    while True:
+        for file_name in sorted(glob(join(RECORDINGS_DIR, "*.wav"))):
+            print("process file: ", file_name)
+            with multiprocessing.Pool(1) as pool:
+                pool.map(transcribe, (file_name,))
+
+        await asyncio.sleep(0.01) # small idle for the cpu
+
+
+def transcribe(file_name):
+    result = model.transcribe(file_name)
+
+    print("transcribe result: ", result)
+
+    remove_audio(file_name)
+
+    final_text = process_recorded_speech(result["text"])
+    if final_text in IGNORED_WORDS:
+        print(f"Skipping: {final_text}")
+        return
+
+    # Type the text
+    pyautogui.typewrite(process_recorded_speech(result["text"]))
+    # Press Enter
+    pyautogui.press('enter')
+
+
+async def main():
+    await asyncio.gather(
+        record_audio(),
+        process_new_audio_files(),
+    )
 
 
 if __name__ == "__main__":
-    while True:
-        audio_data = record_audio()
-
-        if not audio_data:
-            continue
-
-        file_name = f"recorded_audio_{int(time.time())}.wav"
-
-        save_audio(audio_data, file_name)
-
-        result = model.transcribe(file_name)
-
-        remove_audio(file_name)
-
-        final_text = process_recorded_speech(result["text"])
-
-        if final_text in IGNORED_WORDS:
-            continue
-
-        # Type the text
-        pyautogui.typewrite(process_recorded_speech(result["text"]))
-
-        # Press Enter
-        pyautogui.press('enter')
+    asyncio.run(main())
